d9b1a127539abff82f9e476db082e0e91691b711
ggarcez <ggarcez613@gmail.com>
2026-01-19 11:31:27 -0300

    add event list on events and results pages

--- src/app/(public)/(home)/page.tsx
+++ src/app/(public)/(home)/page.tsx
@@ -1,12 +1,13 @@
 import { EventList } from '@/components/event-list'
 import { Hero } from '@/components/hero'
+import { SeeMore } from '@/components/see-more'
 
 export default function HomePage() {
 	return (
 		<>
 			<Hero />
 
-			<EventList />
+			<EventList title="Eventos em Destaque" footerActions={<SeeMore />} />
 		</>
 	)
 }
