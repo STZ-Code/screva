b5daf0aaee38d507c2a7f6fcdb5800c6fbf8eed1
ggarcez <ggarcez613@gmail.com>
2026-06-11 00:35:31 -0300

    remove mocks

--- src/lib/get-current-user.ts
+++ src/lib/get-current-user.ts
@@ -1,4 +1,8 @@
-import { cookies } from 'next/headers'
+import {
+	// cookies,
+	headers,
+} from 'next/headers'
+import { authClient } from './auth-client'
 
 export type Role = 'ADMIN' | 'CUSTOMER'
 
@@ -11,60 +15,60 @@ export type User = {
 	role: Role
 }
 
-const users = [
-	{
-		name: 'Felipe Admin',
-		email: 'felipe-admin@teste.com',
-		emailVerified: true,
-		id: 'tester-1',
-		avatarUrl: null,
-		role: 'ADMIN',
-	},
-	{
-		name: 'Felipe Cliente',
-		email: 'felipe-cliente@teste.com',
-		emailVerified: true,
-		id: 'tester-2',
-		avatarUrl: null,
-		role: 'CUSTOMER',
-	},
-	{
-		name: 'Teste de cliente',
-		email: 'tester@teste.com',
-		emailVerified: true,
-		id: 'tester-3',
-		avatarUrl: null,
-		role: 'CUSTOMER',
-	},
-	{
-		name: 'Administrador Tester',
-		email: 'tester-2@teste.com',
-		emailVerified: true,
-		id: 'tester-3',
-		avatarUrl: null,
-		role: 'ADMIN',
-	},
-]
+// const users = [
+// 	{
+// 		name: 'Felipe Admin',
+// 		email: 'felipe-admin@teste.com',
+// 		emailVerified: true,
+// 		id: 'tester-1',
+// 		avatarUrl: null,
+// 		role: 'ADMIN',
+// 	},
+// 	{
+// 		name: 'Felipe Cliente',
+// 		email: 'felipe-cliente@teste.com',
+// 		emailVerified: true,
+// 		id: 'tester-2',
+// 		avatarUrl: null,
+// 		role: 'CUSTOMER',
+// 	},
+// 	{
+// 		name: 'Teste de cliente',
+// 		email: 'tester@teste.com',
+// 		emailVerified: true,
+// 		id: 'tester-3',
+// 		avatarUrl: null,
+// 		role: 'CUSTOMER',
+// 	},
+// 	{
+// 		name: 'Administrador Tester',
+// 		email: 'tester-2@teste.com',
+// 		emailVerified: true,
+// 		id: 'tester-3',
+// 		avatarUrl: null,
+// 		role: 'ADMIN',
+// 	},
+// ]
 
-async function getUser(email: string) {
-	return {
-		data: {
-			user: users.find((user) => user.email === email),
-		},
-	}
-}
+// async function getUser(email: string) {
+// 	return {
+// 		data: {
+// 			user: users.find((user) => user.email === email),
+// 		},
+// 	}
+// }
 
 export async function getCurrentUser() {
-	const cookie = (await cookies()).get('email')
+	// const cookie = (await cookies()).get('email')
 
-	if (!cookie) return null
-	// const state = await authClient.getSession({
-	// 	fetchOptions: {
-	// 		headers: await headers(),
-	// 	},
-	// })
+	// if (!cookie) return null
+	const state = await authClient.getSession({
+		fetchOptions: {
+			headers: await headers(),
+		},
+	})
 
-	const state = await getUser(cookie.value)
+	// const state = await getUser(cookie.value)
 
 	return state.data?.user as User | undefined
 }
